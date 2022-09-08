38281850ee8de59b7d06e9b20c0fc7105492e9b7
David Landes <davidkailandes@gmail.com>
2022-09-08 11:15:01 -0400

    Initial commit

---
+++ src/pages/Store.tsx
@@ -0,0 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import StoreItems from "../data/items.json";
+//
+export function Store() {
+  return (
+    <>
+      <h1>STORE</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {StoreItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
